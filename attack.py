be1ef28ea2c82e2d0e92fc0a8b55947ff2854c50
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-03-05 11:21:15 +0100

    Clean verifier

--- attack.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from jiarinard.jia_new import jia_binary, jia_new
-
-if __name__ == '__main__':
-    if sys.argv[1] == 'binary':
-        jia_binary(int(sys.argv[2]))
-    else:
-        jia_new(int(sys.argv[2]))
